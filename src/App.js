965af44017eddaef93329d16b78a41cad62e0b95
Akash <ak668463@gmail.com>
2022-08-16 22:49:23 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import {Switch,Route} from 'react-router-dom'
 
 
 function App() {
-  return (
-    <div>
-      Hi
-    </div>
+  return(
+    <Switch>
+      <Route exact path="/">This is homepage</Route>
+      <Route exact path="/starred">This is starred</Route>
+      <Route>This is 404</Route>
+
+    </Switch>
   );
 }
 
